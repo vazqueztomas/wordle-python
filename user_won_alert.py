7b86810ac51a92679075a7497427b8c9e03d5982
Tomas Vazquez <vazquezt2018@gmail.com>
2023-08-17 11:39:04 -0300

    add/ modularized

---
+++ user_won_alert.py
@@ -0,0 +1,9 @@
+
+def user_won_alert(word: str):
+   """
+    Show the won message to the user
+    >>> user_won_alert("QUAKE")
+    You won this game! The word is: QUAKE
+   """
+   print(f'You won this game! The word is: {word}')
+   
